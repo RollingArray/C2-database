d837fd76b8ea3ac6d06f22e8014c80331b6cd41c
Ranjoy <code@rollingarray.co.in>
2021-04-27 16:09:18 +0530

    Update sprint delete query

--- rolli3oh_c2_staging_schema.sql
+++ rolli3oh_c2_staging_schema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Apr 26, 2021 at 03:31 PM
+-- Generation Time: Apr 27, 2021 at 10:35 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.4.2
 
